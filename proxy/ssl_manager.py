406f2b429c7d95d658a0b33a15e14e27d2b35513
wxuycea <numbbvi@naver.com>
2024-05-24 00:25:07 +0900

    Feat: add certificate generation and management functionality

--- proxy/ssl_manager.py
+++ proxy/ssl_manager.py
@@ -1,25 +1,16 @@
 import ssl
-import socket
+from certificate_manager import CertificateManager
 
-class SSLServer:
-    def __init__(self, certfile, keyfile, host, port, context):
-        self.certfile = certfile
-        self.keyfile = keyfile
-        self.host = host
-        self.port = port
-        self.context = self.ssl_context()
-    
-    # SSL 컨텍스트 생성
-    def ssl_context(self):
-        # 컨텍스트 생성 중 버전 설정
-        # Cipher Suite
-    
-    # 클라이언트
-    def handle_client(self):
-        # 네트워크 통신 중 에러처리
-        
-    # 실행
-    def run(self):
-        # 세션 관리(타임아웃 설정)
-    
-# if __name__ == '__main__':
+class SSLManager:
+    def __init__(self):
+        self.cert_manager = CertificateManager()
+
+    def create_ssl_context(self):
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.load_cert_chain(certfile=self.cert_manager.get_cert_path(),
+                                keyfile=self.cert_manager.get_key_path())
+        return context
+
+if __name__ == "__main__":
+    ssl_manager = SSLManager()
+    context = ssl_manager.create_ssl_context()
