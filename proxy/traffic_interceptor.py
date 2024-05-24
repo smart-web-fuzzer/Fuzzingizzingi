55a471dba641167233a89ee017677eda847ec971
kitetae3737 <113299255+kitetae3737@users.noreply.github.com>
2024-05-24 19:26:20 +0900

    Feat: add class for import

--- proxy/traffic_interceptor.py
+++ proxy/traffic_interceptor.py
@@ -3,139 +3,139 @@ import sys
 import socket
 import argparse
 
-# 커맨드로부터 포트번호 받기
-parser = argparse.ArgumentParser()
-parser.add_argument('-p', type=int, help="port number")
-args = parser.parse_args()
-my_port = args.p
-if(my_port):
-    pass
-else:
-    print("[!] python <file.py> -p <port> 형식으로 접속해주세요!")
-    sys.exit()
+class TrafficIntercept:
+    def __init__(self, port):
+        self.port = port
+        self.cache = {}
+        self.http_methods = ["GET", "POST", "DELETE", "PUT", "PATCH", "HEAD", "OPTION", "TRACE"]
+        self.server_socket = None
 
-# 터미널창 비우기 (Windows용, Linux/Mac에서는 'clear' 사용)
-os.system('cls' if os.name == 'nt' else 'clear')
-print("Proxy Started\n")
+    def start(self):
+        # 터미널창 비우기 (Windows용, Linux/Mac에서는 'clear' 사용)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Proxy Started\n")
 
-# Cache 사전
-cache = {}
-# HTTP 메소드 종류
-http = ["GET", "POST", "DELETE", "PUT", "PATCH", "HEAD", "OPTION", "TRACE"]
+        # 사용자와 연결하기 위한 소켓 생성
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind(('', self.port))
+        self.server_socket.listen(10)  # 리슨
+        print(f">> [{self.port}] Listening...\n")
+        client_socket, addr = self.server_socket.accept()  # 연결
+        print(">> Connected!\n")
 
-# 사용자와 연결하기 위한 소켓 생성
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', my_port))
-s.listen(10)  # 리슨
-print(f">> [{my_port}] Listening...\n")
-c, addr = s.accept()  # 연결
-print(">> Connected!\n")
+        while True:
+            req = client_socket.recv(65535).decode()  # 요청 패킷 가져와서 디코딩
 
-while True:
-    req = c.recv(65535).decode()  # 요청 패킷 가져와서 디코딩
+            # telnet에서 엔터쳤을 때 IndexError방지
+            try:
+                parts = req.split()
+                if len(parts) == 1 and parts[0].startswith('http'):
+                    msg = "GET"
+                    url = parts[0]
+                else:
+                    msg = parts[0]  # 메소드
+                    url = parts[1]  # url
+            except:
+                info = "[!] Send some Request!\n"
+                client_socket.send(info.encode())
+                continue
 
-    # telnet에서 엔터쳤을 때 IndexError방지
-    try:
-        parts = req.split()
-        if len(parts) == 1 and parts[0].startswith('http'):
-            msg = "GET"
-            url = parts[0]
-        else:
-            msg = parts[0]  # 메소드
-            url = parts[1]  # url
-    except:
-        info = "[!] Send some Request!\n"
-        info = info.encode()
-        c.send(info)
-        continue
+            try:
+                index = '/'.join(url.split("/")[3:])  # 파일경로
+                root = url.split("/")[2]  # IP/도메인
+            except:
+                index = ''
+                root = url
 
-    try:
-        index = '/'.join(url.split("/")[3:])  # 파일경로
-        root = url.split("/")[2]  # IP/도메인
-    except:
-        index = ''
-        root = url
+            if msg != "GET":  # GET이 아닌 경우
+                self.handle_non_get_requests(client_socket, msg)
+                continue
+            else:  # GET인 경우
+                self.handle_get_request(client_socket, req, root, index)
 
-    if msg != "GET":  # GET이 아닌 경우
-        if msg in http:
+    def handle_non_get_requests(self, client_socket, msg):
+        if msg in self.http_methods:
             info = "[!] Not Implemented(501)!\n"
         else:
             info = "[!] Bad Request(400)!\n"
         print(info)
-        info = info.encode()
-        c.send(info)
+        client_socket.send(info.encode())
         print(">> Keep going...\n")
-        continue
-    else:  # GET인 경우
+
+    def handle_get_request(self, client_socket, req, root, index):
         print("* * * Request * * *\n")
         print(req + "\n")
 
-        # root + index
         target = root + "/" + index
 
-        # Cache가 있는 경우
-        if target in cache:
-            get = cache[target]
-            get = get.replace("$$$", ":")
-            get = get.replace("###", "{")
-            get = get.replace("@@@", ",")
-            get = get.encode()
-            c.sendall(get)
-            print(">> Cache Used!\n")
-            continue
+        if target in self.cache:
+            self.send_cached_response(client_socket, target)
+            return
 
-        # 절대경로 -> 상대경로
         new_req = f"GET /{index} HTTP/1.1\r\nHost: {root}\r\nConnection: close\r\n\r\n"
         new_req = new_req.encode()
 
-        # 오리진 서버와 연결하기 위한 소켓 생성
         s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             host = socket.gethostbyname(f"{root}")
         except:
             info = "[!] There's no such Host!\n"
             print(info)
-            info = info.encode()
-            c.send(info)
+            client_socket.send(info.encode())
             print(">> Keep going...\n")
-            continue
+            return
+
         host_port = 80
         s2.connect((host, host_port))
-
-        # 오리진 서버에 요청 패킷 전송
         s2.sendall(new_req)
-        value = ""  # 응답 패킷 내용을 저장할 빈 문자열
+        value = ""
         bad = False
 
         while True:
-            res = s2.recv(65535)  # 응답 가져오기
+            res = s2.recv(65535)
             if not res:
                 break
-            # 응답 패킷을 Cache 사전에 저장하기 위해 디코딩
             deco = res.decode()
             deco = deco.replace(":", "$$$")
             deco = deco.replace("{", "###")
             deco = deco.replace(",", "@@@")
-            value += deco  # 문자열에 패킷 내용 저장
+            value += deco
             if deco.split()[1] == "400":
                 info = "[!] Bad Request(400)!\n"
                 print(info)
-                info = info.encode()
-                c.send(info)
+                client_socket.send(info.encode())
                 bad = True
                 break
-            # 응답 패킷을 사용자에게 전송
-            c.sendall(res)
+            client_socket.sendall(res)
         print(">> Send Success!\n")
 
         if bad:
             print(">> Keep going...\n")
-            continue
+            return
 
-        # Cache 저장
-        cache[target] = value
+        self.cache[target] = value
         print(">> New Cache Written!\n")
         print("* * * Cache List * * *")
-        for index, key in enumerate(cache.keys()):
+        for index, key in enumerate(self.cache.keys()):
             print(f"[{index + 1}] {key}")
         print("\n>> Keep going...\n")
+
+    def send_cached_response(self, client_socket, target):
+        get = self.cache[target]
+        get = get.replace("$$$", ":")
+        get = get.replace("###", "{")
+        get = get.replace("@@@", ",")
+        client_socket.sendall(get.encode())
+        print(">> Cache Used!\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', type=int, help="port number")
+    args = parser.parse_args()
+    my_port = args.p
+    if my_port:
+        server = ProxyServer(my_port)
+        server.start()
+    else:
+        print("[!] python <file.py> -p <port> 형식으로 접속해주세요!")
+        sys.exit()
