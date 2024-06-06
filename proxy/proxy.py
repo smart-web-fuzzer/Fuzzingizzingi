b47f8fbe2f7ba94b499fe8175f7692f916ed3f3b
wxuycea <numbbvi@naver.com>
2024-06-07 04:03:09 +0900

    Fix: modify code to connect to proxy server

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -1,27 +1,28 @@
 import sys
 import signal
 from network_listener import NetworkListener
-from traffic_interceptor import TrafficIntercept
-from request_modifier import HTTPRequest
-from logger import Logger
+# from traffic_interceptor import TrafficIntercept
+# from request_modifier import HTTPRequest
 # from performance import
 
-def signal_handler(signal, frame):
-    print('Signal received, stopping server.')
-    server.stop_server()
-    sys.exit(0)
+def signal_handler(server):
+    def handler(signal, frame):
+        print('Signal received, stopping server.')
+        # 서버 중지
+        server.stop_server()
+        sys.exit(0)
+    return handler
 
 if __name__ == "__main__":
-    # Initialize logger
     logger = Logger()
     logger.log("Starting the server...")
 
-    # Start the network listener
     port = 8888
     server = NetworkListener(port=port, logger=logger)
-    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGINT, lambda s, f: signal_handler(s, f, server))
     
     try:
         server.start_server()
     except Exception as e:
         logger.log("Server encountered an error and stopped.")
+        logger.log(str(e))
